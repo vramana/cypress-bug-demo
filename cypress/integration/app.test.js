66cbc83ab4c8d52b9a9caf47fbcccfafd76784b3
Sharan-ram <sharanram1993@gmail.com>
2019-11-26 14:53:51 +0530

    Add 1st test for menu

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,6 +1,11 @@
 describe("Testing", () => {
   it("Check", () => {
     cy.visit("/");
-    cy.contains("Welcome to app");
+    cy.contains("FOODS").click();
+    cy.get("[role=menu]")
+      .contains("Pork")
+      .click();
+    cy.get("[role=menu]").should("not.exist");
+    cy.contains("Pork");
   });
 });
--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Button from "@material-ui/core/Button";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 function App() {
-  return <div className="App">Welcome to app</div>;
+  const [dialog, setDialog] = useState(false);
+  const [selectedFood, setSelectedFood] = useState();
+  const foods = ["Chicken", "Mutton", "Beef", "Pork"];
+  return (
+    <div>
+      {!dialog && <Button onClick={() => setDialog(true)}>FOODS</Button>}
+      <Menu open={dialog}>
+        {foods.map((food, index) => (
+          <MenuItem
+            onClick={() => {
+              setSelectedFood(index);
+              setDialog(false);
+            }}
+          >
+            {food}
+          </MenuItem>
+        ))}
+      </Menu>
+      {selectedFood !== undefined && (
+        <div>
+          The food you have selected is <span>{foods[selectedFood]}</span>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
