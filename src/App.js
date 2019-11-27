543f3ee0adfd4f70a01f6a38fa9d398633545ddc
Sharan-ram <sharanram1993@gmail.com>
2019-11-27 18:46:42 +0530

    Add AutoComplete component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,221 @@
-import React, { useState } from "react";
-
-import Button from "@material-ui/core/Button";
-import MenuItem from "@material-ui/core/MenuItem";
+import React, { useRef } from "react";
+import matchSorter from "match-sorter";
 import Paper from "@material-ui/core/Paper";
 import Popper from "@material-ui/core/Popper";
+import TextField from "@material-ui/core/TextField";
+
 import { withStyles } from "@material-ui/core/styles";
 
+import Downshift from "downshift";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const BodyText = ({ children }) => children;
+
 const styles = {
+  container: {
+    marginLeft: "300px"
+  },
   popper: {
     zIndex: 1500
   }
 };
 
-function _App({ classes }) {
-  const [dialog, setDialog] = useState(false);
-  const [selectedFood, setSelectedFood] = useState();
-  const foods = ["Chicken", "Mutton", "Beef", "Pork"];
+const scrollableMaxResults = 250;
+
+function Menu({ downshiftProps, ...props }) {
+  const {
+    classes,
+    dataSource,
+    dataSourceConfig,
+    maxSearchResults,
+    scrollable
+  } = props;
+
+  const searchResults = scrollable ? scrollableMaxResults : maxSearchResults;
+
+  const {
+    getItemProps,
+    getMenuProps,
+    isOpen,
+    inputValue,
+    highlightedIndex
+  } = downshiftProps;
+
+  let items = matchSorter(dataSource, inputValue, {
+    keys: [dataSourceConfig.text]
+  }).slice(0, searchResults);
+  items = items.map((item, index) => {
+    const isHighlighted = highlightedIndex === index;
+    const props = {
+      ...getItemProps({ item }),
+      key: index,
+      selected: isHighlighted
+    };
+
+    return (
+      <MenuItem {...props}>
+        <BodyText>{item[dataSourceConfig.text]}</BodyText>
+      </MenuItem>
+    );
+  });
+
+  const popperNode = props.popperRef.current;
+
   return (
-    <div>
-      {!dialog && <Button onClick={() => setDialog(true)}>FOODS</Button>}
-      <Popper data-test-id="menu" open={dialog} className={classes.popper}>
-        <div>
-          <Paper>
-            {foods.map((food, index) => (
-              <MenuItem
-                onClick={() => {
-                  setSelectedFood(index);
-                  setDialog(false);
-                }}
-              >
-                {food}
-              </MenuItem>
-            ))}
-          </Paper>
-        </div>
-      </Popper>
-      {selectedFood !== undefined && (
-        <div>
-          The food you have selected is <span>{foods[selectedFood]}</span>
+    <Popper
+      open={isOpen}
+      anchorEl={popperNode}
+      placement="bottom"
+      modifiers={{
+        flip: { enabled: false },
+        hide: { enabled: false },
+        preventOverflow: { enabled: false }
+      }}
+      className={classes.popper}
+      data-test-id="autoCompleteMenu"
+    >
+      <div {...(isOpen ? getMenuProps({}, { suppressRefError: true }) : {})}>
+        <Paper square>{items}</Paper>
+      </div>
+    </Popper>
+  );
+}
+
+function Field({ downshiftProps, ...props }) {
+  const { classes, search, clear, popperRef, TextFieldProps } = props;
+
+  const { inputValue, getInputProps, openMenu } = downshiftProps;
+
+  const clearInputAdornment = null;
+
+  const textFieldProps = {
+    className: search === true ? classes.search : "",
+    ...TextFieldProps,
+    InputProps: {
+      ...TextFieldProps.InputProps,
+      ...getInputProps({ onFocus: openMenu }),
+      inputRef: popperRef,
+      endAdornment: clear && inputValue !== "" ? clearInputAdornment : undefined
+    }
+  };
+
+  return <TextField {...textFieldProps} />;
+}
+
+const defaultItem = {};
+
+const autoCompleteStyles = {
+  search: {
+    minHeight: 50,
+    justifyContent: "center"
+  },
+  selectedItem: { fontWeight: 500 },
+
+  popper: { zIndex: 1500 },
+
+  container: { position: "relative" },
+
+  fullWidthContainer: {
+    position: "relative",
+    width: "100%"
+  },
+  icon: {
+    paddingRight: 5,
+    "&:hover": {
+      backgroundColor: "transparent"
+    }
+  },
+
+  paper: {
+    position: "absolute",
+    zIndex: 3,
+    left: 0,
+    right: 0
+  },
+  bodyText: {
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
+    overflow: "hidden"
+  }
+};
+
+function _AutoComplete(props) {
+  const popperRef = useRef();
+
+  const {
+    classes,
+    TextFieldProps,
+    dataSourceConfig,
+    defaultSelectedItem,
+    variant = "default",
+    value,
+    onChange,
+    onInputChange
+  } = props;
+  const itemToString = item => (item && item[dataSourceConfig.text]) || "";
+
+  let _props;
+  if (variant === "next") {
+    _props = { selectedItem: value || defaultItem };
+  } else {
+    _props = { initialSelectedItem: defaultSelectedItem };
+  }
+
+  return (
+    <Downshift
+      onChange={onChange}
+      onInputValueChange={onInputChange}
+      itemToString={itemToString}
+      {..._props}
+    >
+      {downshiftProps => (
+        <div
+          className={
+            TextFieldProps.fullWidth !== false
+              ? classes.fullWidthContainer
+              : classes.container
+          }
+        >
+          <Field
+            downshiftProps={downshiftProps}
+            popperRef={popperRef}
+            {...props}
+          />
+          <Menu
+            downshiftProps={downshiftProps}
+            popperRef={popperRef}
+            {...props}
+          />
         </div>
       )}
+    </Downshift>
+  );
+}
+
+const AutoComplete = withStyles(autoCompleteStyles)(_AutoComplete);
+
+AutoComplete.defaultProps = {
+  maxSearchResults: 5,
+  onChange: () => {}
+};
+
+function _App({ classes }) {
+  const foods = [
+    { name: "Chicken" },
+    { name: "Mutton" },
+    { name: "Beef" },
+    { name: "Pork" }
+  ];
+  return (
+    <div className={classes.container}>
+      <AutoComplete
+        dataSource={foods}
+        dataSourceConfig={{ value: "name", text: "name" }}
+        TextFieldProps={{
+          name: "foods",
+          fullWidth: false
+        }}
+      />
     </div>
   );
 }
