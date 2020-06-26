d9151ace8dd3de64009db65df58c47883ecc1ea1
Ramana Venkata <vramana@users.noreply.github.com>
2020-06-26 12:42:09 +0530

    Format test

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,10 +1,10 @@
-describe("Testing", () => {
-  it("Check", () => {
-    cy.visit("/");
-    cy.get("[name=foods]")
-      .type("Beef");
-    cy.contains('[role=option]',  "Beef")
-      .click();
-    cy.get('[name=foods]').should('have.value', "Beef")
+describe('Testing', () => {
+  it('Check', () => {
+    cy.visit('/');
+
+    cy.get('[name=foods]').type('Beef');
+    cy.contains('[role=option]', 'Beef').click();
+
+    cy.get('[name=foods]').should('have.value', 'Beef');
   });
 });
