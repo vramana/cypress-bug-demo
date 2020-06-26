abc1aa6030998dda5c7321359b1950cb918646b5
Ranjith Nori <ranjithnori@gmail.com>
2020-06-26 12:45:33 +0530

    Workaround

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,10 +1,7 @@
 describe('Testing', () => {
   it('Check', () => {
     cy.visit('/');
-
-    cy.get('[name=foods]').type('Beef');
-    cy.contains('[role=option]', 'Beef').click();
-
+    cy.get('[name=foods]').type('Beef{downarrow}{enter}');
     cy.get('[name=foods]').should('have.value', 'Beef');
   });
 });
